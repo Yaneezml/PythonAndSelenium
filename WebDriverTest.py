06abdc5fde1440985ababeb6e539be1ed42e90c7
Yaneezml <yaneezlowtun1@hotmail.co.uk>
2018-01-31 17:24:33 +0000

    python selenium first test

---
+++ WebDriverTest.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def main():
+    browser = webdriver.Chrome()
+    browser.maximize_window()
+    browser.get('https://configurator.astonmartin.com/#/')
+
+    wait = WebDriverWait(browser, 15)
+    assert "astonmartin" in browser.current_url
+
+    """Closes the 'View Policy' frame"""
+    policy = "button[ng-click='closePolicy()']"
+    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, policy)))
+    browser.find_element_by_css_selector(policy).click()
+
+    """Finds current country selected """
+    currentCountry = browser.find_element_by_css_selector('p.overlay-item-active').text
+    print("This is the country selected: " + currentCountry)
+    assert (currentCountry == 'United States'), ("Current Selection: " + currentCountry)
+
+    print(selectCountry(browser, "United Kingdom"))
+    # browser.quit()
+
+
+def selectCountry(driver, country):
+    assert (driver.find_element_by_css_selector('p.overlay-item-active').text != country), (country + "is already "
+                                                                                                  "selected!")
+
+    list_of_countries = []
+    cText = driver.find_elements_by_css_selector("p.overlay-item")
+    for i in cText:
+        list_of_countries.append(i.text)
+    return list_of_countries
+
+
+if __name__ == "__main__":
+    main()
